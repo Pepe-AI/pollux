2f5b87df5dd019c1f61ee80dadd6a12e8440c71a
Pepe-AI <68040297+Pepe-AI@users.noreply.github.com>
2024-07-14 18:18:16 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $( document ).ready(function() {
 
     var envelope = $('#envelope');
@@ -24,31 +23,3 @@ $( document ).ready(function() {
            .removeClass("open");
     }
 
-=======
-$( document ).ready(function() {
-
-    var envelope = $('#envelope');
-    var btn_open = $("#open");
-    var btn_reset = $("#reset");
-
-    envelope.click( function() {
-        open();
-    });
-    btn_open.click( function() {
-        open();
-    });
-    btn_reset.click( function() {
-        close();
-    });
-
-    function open() {
-        envelope.addClass("open")
-           .removeClass("close");
-    }
-    function close() {
-        envelope.addClass("close")
-           .removeClass("open");
-    }
-
->>>>>>> d247001b70ec225bbcf7193a88ab049330a048de
-});
